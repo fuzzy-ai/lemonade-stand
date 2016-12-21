7e3d7da7b358623e46072fd81469a515922b27a4
Pablo Boerr <pablo@fliawebdesign.com>
2016-12-21 12:02:29 -0500

    animations, added IDs, GSAP.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,16 +1,18 @@
 // Lemonade stand
 
-
+(function(){
 let cloudyIcon = document.querySelector('.cloudy-icon-btn'),
     sunnyIcon = document.querySelector('.sunny-icon-btn'),
     b = document.getElementsByTagName('body')[0],
     bSunny = document.getElementsByClassName('sunny'),
     sun = document.querySelector('.sun'),
     sunnyOrCloudy = document.querySelector('.sunny-or-cloudy'),
-    jsNotActive = document.querySelector('.js-not-active');
+    jsNotActive = document.querySelector('.js-not-active'),
+    lemonHeading = document.querySelector('.lemonsplanation__heading'),
+    lemonPara = document.querySelector('.lemonsplanation__para');
 
-const startBtn = document.querySelector('.js-start-btn');
-const sceneAction = document.querySelector ('.scene-action');
+const startBtn = document.querySelector('.js-start-btn'),
+      sceneAction = document.querySelector ('.scene-action');
 
 
 //toggle cloudy or sunny weather
@@ -43,6 +45,13 @@ sunnyIcon.addEventListener('click', function(){
 
 //GSAP animations
 
+tlIntroText = new TimelineMax();
+
+  tlIntroText
+  .from(lemonHeading, .5, {autoAlpha: 0, y: -20, ease: Power2.easeIn})
+  .from(lemonPara, .5, {autoAlpha: 0, ease: Power4.easeIn})
+  .fromTo(startBtn, 2, {autoAlpha: 0, scale:0, yPercent: '-100'},{autoAlpha:1, scale:1, ease: Back.easeIn}, '-=.25');
+
 
 TweenMax.from(".sun", 1, {opacity:0, rotation:360,  scale:0, ease: Power4.easeInOut})
 TweenMax.to(".sun", 2, {opacity:1, y:0,   scale:1.1, ease: Power2.easeInOut})
@@ -51,16 +60,18 @@ TweenMax.staggerFrom(".clouds", 3, {cycle:{
 }, autoAlpha:0,  ease:  Power1.easeOut }, 1)
 
 startBtn.onclick = function() {
-  TweenMax.to(".lemonsplanation", 3, {scale: 0, opacity:0, ease: Power4.easeInOut})
+  TweenMax.to(".lemonsplanation", 3, {scale: 0, opacity:0, x:-100, ease: Power4.easeInOut})
   TweenMax.to(window, 1, {scrollTo:{y:"#funtimes", offsetY:-100}, onComplete:function(){
     jsNotActive.classList.remove('js-not-active');
     sceneAction.classList.add('js-active');
+    // TweenMax.staggerFrom(".buyer" , 1, {opacity: 0,  x:-200})
+    // TweenMax.staggerTo(".buyer" , 3, {opacity:1, x:0, ease:  Power0.easeIn }, 4.5)
 
-    var tl = new TimelineLite();
+    let tl = new TimelineLite();
       tl.to(".buyer", 3, {left:"30%"})
         .add("purchasing", '+=2')
         .to(".buyer", 9, {x:2000}, "purchasing");
-    }
-});
-
-}
+      }
+    });
+  }
+})()
