73a5463a6e13dfbb3929b0c03f088850dfb6036e
Pablo Boerr <pablo@fliawebdesign.com>
2017-01-06 00:22:00 -0500

    Tidied up SVG animations  + sass mods + ForeignObject Mods
    Tidied SVG Animation
    Added visual cues on purchase or no purchase
    Removed ForeignObject from SVG

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -116,6 +116,7 @@ var buyer = function () {
 (function(){
   console.log(state);
   let cloudyIcon = document.querySelector('.cloudy-icon-btn'),
+      settings = document.querySelector(".settings"),
       addBuyerLink = document.querySelector("#add-buyer-link"),
       stand = document.querySelector('#stand'),
       snowMount = document.querySelector('#snow'),
@@ -152,7 +153,9 @@ var buyer = function () {
       snowmanLeftEye = document.querySelector("#lefteye"),
       snowmanRightEye = document.querySelector("#righteye"),
       star = document.querySelector("#star"),
-      star2 = document.querySelector("#star-2")
+      star2 = document.querySelector("#star-2"),
+      buying = document.querySelector("#buying"),
+      noBuying = document.querySelector("#buying-no"),
       intFrameWidth = window.innerWidth;
       //buyer = [boy, girl];
 
@@ -166,13 +169,12 @@ var buyer = function () {
   mainTl = new TimelineMax();
 
   function setCloudyWeather() {
+    state.setSunny(0);
     b.classList.toggle('cloudy');
     b.classList.remove('sunny')
     cloudyIcon.classList.add('active-icon');
     sunnyIcon.classList.remove('active-icon');
     sunnyOrCloudy.innerHTML = 'cloudy';
-
-    state.setSunny(0);
 
     window.setTimeout(function(){
       sun.classList.add('offset-sun');
@@ -252,6 +254,7 @@ function nextBuyer() {
     animateBuyer(state.buyers[0]);
   } else {
     setTimeout(nextBuyer, 1000);
+    buying.classList.remove("buying-active");//remove checkmark when no more buyers in queue
   }
 }
 
@@ -272,50 +275,62 @@ function animateBuyer(buyer) {
     }
   });
 
+
+
 function purchase(){
   if (state.didPurchase == true){
     console.log("buying");
+    buying.classList.add("buying-active");
     tlBuyers.resume();
   } else {
     console.log("im out of here")
+    noBuying.classList.add("no-buying-active");
     tlBuyers.reverse();
   }
 }
+
   let buyerEl = document.querySelector('.' + buyer.gender);
   tlBuyers
-  .set(buyerEl, {x: -`${walkingDist}` * 4, force3D:true, y:20, scale:1.15})
-  .set(stand, {y: 70})
+  .set(buying, {className:"-=buying-active"})
+  .set(noBuying, {className:"-=no-buying-active"})
+  .set(chocoCup, {className:"-=choco-cup-bought"})
+  .set(chocoCup, {className:"+=choco-cup-noshow"})
+  .set(buyerEl, {x: -`${walkingDist}` * 4, force3D:true, y:24, scale:1.20})
+  .set(stand, {y: 65})
   .set(snowMid, { x: 200})
   .set(chocoSteam, {autoAlpha:0.5, y: -2})
   .set(snow, {y: 42})
+  .set(lActionArm, {rotation:0})
   .to(chocoSteam, 1, {y:-10, autoAlpha:0.65, repeat:3, delay: 0.25})
-  .to(buyerEl, 2.5, {scale:1, x: `${walkingDist}`, delay:4, ease:Power1.easeInOut, onComplete: function() { console.log('evaluate now'); buyer.evaluate()}}, "-=2.5")
+  .to(buyerEl, 2.5, {scale:1, x: `${walkingDist}`, delay:3, ease:Power1.easeInOut, onComplete: function() { console.log('evaluate now'); buyer.evaluate()}}, "-=2.5")
   .add("ThinkingAboutIt")
    tlBuyers
-   .addPause("ThinkingAboutIt+=4", purchase)
-
-  .fromTo(chocoCupTable, 2.25, {x:160, autoAlpha:0.9}, {x:2, autoAlpha:1, ease: Back.easeInOut}, "-=0.75")
+   .addPause("ThinkingAboutIt+=4", purchase)//pass the purchase function here
+   .set(buying, {className:"-=buying"})
+   .set(noBuying, {className:"-=buying-no"})
+  .fromTo(chocoCupTable, 2.25, {x:160, autoAlpha:0.9}, {x:-3, autoAlpha:1, ease: Back.easeInOut}, "-=0.75")
   .add("chocoServed")
-  .to(chocoSteam, 1, {y:-14, autoAlpha:0.8})// <= evaluate here
-  .to(lActionArm , 0.8, {  rotation:-90, transformOrigin:"top top", ease: Power4.easeIn, onComplete:   function addCup(){
-    chocoCup.classList.remove("choco-cup-noshow");
-    chocoCup.classList.add("choco-cup-bought");
+  .to(chocoSteam, 1, {y:-14, autoAlpha:0.8})
+  .to(lActionArm , 0.8, {  rotation:-90, transformOrigin:"top top", ease: Power4.easeIn, onComplete: function addCup(){
+    chocoCup.classList.toggle("choco-cup-noshow");
+    chocoCup.classList.toggle("choco-cup-bought");
   }})
   .to(snowmanArm, 0.25, {rotation:2, y: '+=1px', x: "+=1px", transformOrigin: "top leftt", repeat: 3, yoyo: true}, "chocoServed+=.5")
   .fromTo(star, 0.2, {fill:'#E1D9CE'}, {fill: '#A36B92', repeat: 4, yoyo: true}, "-=.4")
   .fromTo(star2, 0.2, {fill:'#A36B92'}, {fill: '#E1D9CE', repeat: 4, yoyo: true}, "-=.4")
-  // .to(snowmanLeftEye, .75, { x: 4, scale: 1.1 })
-  // .to(snowmanRightEye, .75, { x: 4, scale: .95 })
   .to(chocoCupTable,0.5, {autoAlpha:0})
-  .to(buyerEl, 4, { scale: .95,  x: `${walkingDist}` * 4, ease:Power4.easeIn})
-  .to(chocoCup, 0.05, {autoAlpha:0});
+  .to(buyerEl, 4, { scale: .95,  x: `${walkingDist}` * 4, ease:Power4.easeIn});
 }
+
 function getStartSceneTl(){
 
 let startScene = new TimelineMax();
   //start and load animation loop
+  TweenMax.set(settings, {autoAlpha:0, scale:0, height: 0})
+
   startBtn.addEventListener('click' , function() {
     TweenMax.to(lemonsplanation, 3, {scale: 0, opacity:0, x:-100, ease: Power4.easeInOut})
+
 
       $.post('/data/seller', function(data) {
         for (var i = 0; i < 10; i++) {
@@ -326,21 +341,13 @@ let startScene = new TimelineMax();
       });
       TweenMax.set(sceneAction, { autoAlpha:0})
       TweenMax.to(".lemonsplanation", 3, {scale: 0, opacity:0, ease: Power4.easeInOut})
-      TweenMax.to(window, 1, {scrollTo:{y:"#funtimes", offsetY:-300}, onComplete:function(){
+      TweenMax.to(window, 1, {scrollTo:{y:"#funtimes", offsetY:-580, ease:Back.easeInOut}, onComplete:function(){
       jsNotActive.classList.remove('js-not-active');
       sceneAction.classList.add('js-active');
       TweenMax.to(sceneAction, 1.75, {autoAlpha:1})
+      TweenMax.to(settings, .75, {autoAlpha:1, scale:1, height: "100%"})
 
       nextBuyer();
-// var buyer = state.buyers[0]; buyer.evaluate();
-
-      //buyer loop
-      /*
-      buyers.forEach(function(buyer){
-        console.log("hrm", state);
-
-
-      })*/
       }
     });
   });
